d46f0f4e453d1e3df7a132f963094921f30b4d98
Ruslan Hladchenko <rugladchenko@gmail.com>
2024-02-13 23:43:49 +0200

    Add demo example (without h2)

---
+++ src/main/java/com/bibernate/example/entity/Notes.java
@@ -0,0 +1,22 @@
+package com.bibernate.example.entity;
+
+import com.breskul.bibernate.annotation.Entity;
+import com.breskul.bibernate.annotation.Id;
+import com.breskul.bibernate.annotation.ManyToOne;
+import com.breskul.bibernate.annotation.Table;
+import com.breskul.bibernate.demo.entity.Person;
+
+
+@Entity
+@Table(name = "notes")
+public class Notes {
+
+  @Id
+  private Long id;
+
+  private String note;
+
+  @ManyToOne
+  private Person persons;
+
+}
