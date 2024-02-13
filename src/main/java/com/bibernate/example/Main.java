666a81386048810b2420fe75da5a1cad4ab56b8d
Ruslan Hladchenko <rugladchenko@gmail.com>
2024-02-14 00:09:33 +0200

    Add utility method printPersons

--- src/main/java/com/bibernate/example/Main.java
+++ src/main/java/com/bibernate/example/Main.java
@@ -4,12 +4,16 @@ import com.breskul.bibernate.demo.entity.Person;
 import com.breskul.bibernate.persistence.Persistence;
 import com.breskul.bibernate.persistence.Session;
 import com.breskul.bibernate.persistence.SessionFactory;
+import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) {
     SessionFactory sessionFactory = Persistence.createSessionFactory();
     try (Session session = sessionFactory.openSession()) {
+
+      printPersons(session);
+
       // Create
       Person person = new Person("Taras", "TEST", 20);
       session.persist(person);
@@ -27,5 +31,17 @@ public class Main {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private static void printPersons(Session session) {
+    String query = "SELECT * FROM persons";
+    List<Person> personList = session.executeNativeQuery(query, Person.class);
+    System.out.println("All persons");
+    System.out.println("-----------------------------------------------------------");
+    for (Person person : personList) {
+      System.out.println(person);
+    }
+    System.out.println("-----------------------------------------------------------");
+    System.out.println();
   }
 }
